a2765bcacc829384688c341690246147bf94ccee
yijingyan2 <yijingyan@microsoft.com>
2026-01-20 03:52:17 +0000

    set a 10 days window for the upload
    Signed-off-by: yijingyan2 <yijingyan@microsoft.com>

--- azure-pipelines/scripts/publish-github-prs.py
+++ azure-pipelines/scripts/publish-github-prs.py
@@ -27,6 +27,7 @@ if 'TIMEDELTA_IN_MINUTES' in os.environ and os.environ['TIMEDELTA_IN_MINUTES']:
     timedelta_in_minutes = max(int(os.environ['TIMEDELTA_IN_MINUTES']), 30)
     delta =  datetime.timedelta(minutes=timedelta_in_minutes)
 max_timedelta_in_days = 35
+window_in_days = 10
 
 def kusto_ingest(database='build', table='', mapping='', lines=[]):
     now = datetime.datetime.utcnow().isoformat().replace(':','_')
@@ -63,6 +64,7 @@ def update_start_timestamp():
 def get_pullrequests():
     results = []
     start_timestamp = get_start_timestamp()
+    until = min(start_timestamp + datatime.timedelta(days=window_in_days), until)
     print('start: {0}, until: {1}'.format(start_timestamp.isoformat(), until.isoformat()), flush=True)
     query_pattern = '''
     {
